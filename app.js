63d507c5190ab56a7d5217352c0dadbdafff126f
hoangphihong1 <hphoang@knox.edu>
2025-03-19 12:31:38 +0700

    First code App.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const taskRoutes = require('./routes/taskRoutes');
+
+// Init express app
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDb
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use('./api/tasks', taskRoutes)
+
+// Error handling middleware
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).json({ message: 'Server error', error: err.message });
+//   });
+
+//Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
